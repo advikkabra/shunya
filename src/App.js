2fecb25ed37dd4242677f74341ab5016319e232b
Advik Kabra <advikkabra25@gmail.com>
2022-07-23 11:00:30 +0530

    Extension frontend

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import "./App.css";
 import {
-  Box,
+  Container,
   CircularProgress,
   CircularProgressLabel,
   HStack,
+  Text,
+  Heading,
+  Center,
+  Link,
+  VStack,
 } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Box>
-      <HStack>
-        <CircularProgress value={40} color="green.200">
-          <CircularProgressLabel>50kg</CircularProgressLabel>
+    <Container>
+      <Center pt={2}>
+        <Heading fontSize="4xl">Shunya</Heading>
+      </Center>
+      <VStack pt={2}>
+        <CircularProgress value={40} size="100px" color="teal.500">
+          <CircularProgressLabel>
+            <Text fontWeight="bold" fontSize={"xl"}>
+              40
+            </Text>
+          </CircularProgressLabel>
         </CircularProgress>
-      </HStack>
-    </Box>
+        <Heading fontSize="xl">Amount of CO2 emitted this month</Heading>
+      </VStack>
+      <Center pt={2}>
+        <VStack>
+          <Text as="em"> Visit your dashboard to see more </Text>
+          <Link href="http://localhost:3000/" isExternal>
+            Dashboard
+          </Link>
+        </VStack>
+      </Center>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ChakraProvider,
+  extendTheme,
+  withDefaultColorScheme,
+} from "@chakra-ui/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = extendTheme(
+  {
+    fonts: {
+      heading: `'Inter', sans-serif`,
+      body: `'Inter', sans-serif`,
+    },
+  },
+  withDefaultColorScheme({ colorScheme: "teal" })
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
