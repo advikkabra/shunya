eb81cfb9c1ec436d85359bb99d0c47b3c901cefd
Advik Kabra <advikkabra25@gmail.com>
2022-07-23 13:03:54 +0530

    ext

--- public/flights.js
+++ public/flights.js
@@ -1,8 +1,11 @@
+alert("Working");
+
 if (
   document.location.href.search("flight.easemytrip.com") &&
   document.location.href.search("Review") &&
   document.location.href.search("Checkout")
 ) {
+  alert("hello");
   let orgs = document.querySelectorAll("[ng-bind=\"l.org.split('|')[0]\"]");
   orgs = orgs.slice(0, Math.ceil(orgs.length / 2));
   let dests = document.querySelectorAll("[ng-bind=\"l.dest.split('|')[0]\"]");
